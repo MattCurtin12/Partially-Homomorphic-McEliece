ead893d37e61b4dbadfed310daa39acdd4a18ac2
Matt Curtin <matthew.curtin2@gmail.com>
2022-01-19 02:28:24 -0600

    Cleaned up example. Cleaned up output. Save key gen to files

--- example.c
+++ example.c
@@ -11,6 +11,8 @@ void otp(unsigned char *out, unsigned long long length, unsigned char *in1, unsi
 unsigned char entropy_input[48];
 unsigned char seed[48];
 
+
+
 int main(){
 	
 	//Entropy and seed
@@ -21,9 +23,12 @@ int main(){
 
         randombytes(seed, 48);
 	randombytes_init(seed, NULL, 256);
-	
-	
-	//Public key, private key pair
+
+
+
+
+
+	//Public key, private key pair generation
 	unsigned char *pk, *sk;
 	pk = malloc(crypto_kem_PUBLICKEYBYTES);
     	sk = malloc(crypto_kem_SECRETKEYBYTES);
@@ -33,18 +38,58 @@ int main(){
 		fprintf(stderr, "crypto_kem_keypair\n");
 		return 1;
 	}
+	
+	//Output key pair to files
+	FILE *pk_file = fopen("public_key.txt", "w");
+	FILE *sk_file = fopen("secret_key.txt", "w");
 
-	//Message 1
-	unsigned char *m1, *c1;		//Arbitrary message
-	unsigned char *ss1, *k1;	//shared secret key: plain, encrypted
-	m1  = malloc(crypto_kem_BYTES);
+	fprintBstr(pk_file, "", pk, crypto_kem_PUBLICKEYBYTES);
+	fprintBstr(sk_file, "", sk, crypto_kem_SECRETKEYBYTES);
+
+	fclose(pk_file);
+	fclose(sk_file);
+
+	fprintf(stdout, "Public key outputed to \"public_key.txt\"\n");
+	fprintf(stdout, "Secret key outputed to \"secret_key.txt\"\n\n");
+
+	
+
+
+	
+	//Init plaintexts
+	unsigned char *m1, *m2;
+	m1 = malloc(crypto_kem_BYTES);
+	m2 = malloc(crypto_kem_BYTES);
+
+	//Set m1 = 110, m2 = 101
+	int i;
+	for(i = 0; i < crypto_kem_BYTES - 3; i++){
+		m1[i] = 0;
+		m2[i] = 0;
+	}
+	m1[i] = 1; m2[i] = 1; i++;
+	m1[i] = 1; m2[i] = 0; i++;
+	m1[i] = 0; m2[i] = 1; 
+
+	fprintBstr(stdout, "Original m1: ", m1, crypto_kem_BYTES);
+	fprintBstr(stdout, "Original m2: ", m2, crypto_kem_BYTES);
+	fprintf(stdout, "\n");
+	
+	
+
+
+
+	//Encrypt plaintexts
+	unsigned char *c1, *ss1, *k1;	//ciphertext, plain shared secret key, encrypted key
+	unsigned char *c2, *ss2, *k2;
+
 	c1  = malloc(crypto_kem_BYTES);
 	ss1 = malloc(crypto_kem_BYTES);
-	k1  = malloc(crypto_kem_CIPHERTEXTBYTES);
+	k1  = malloc(crypto_kem_CIPHERTEXTBYTES);	//The encrypted key is the ciphertext in the kem
 
-	//Generate random message 1
-	randombytes(m1, crypto_kem_BYTES);
-	randombytes_init(m1, NULL, 256);
+	c2  = malloc(crypto_kem_BYTES);
+        ss2 = malloc(crypto_kem_BYTES);
+        k2  = malloc(crypto_kem_CIPHERTEXTBYTES);
 
 	//Generate first secret key
 	if( crypto_kem_enc(k1, ss1, pk) != 0){
@@ -55,17 +100,6 @@ int main(){
 	//Encrypt first message with shared secret key using one time pad
 	otp(c1, crypto_kem_BYTES, m1, ss1);
 
-	//Message 2
-	unsigned char *m2, *c2;  	//Arbitrary message
-        unsigned char *ss2, *k2; 	//shared secret key: plain, encrypted
-        m2  = malloc(crypto_kem_BYTES);
-        c2  = malloc(crypto_kem_BYTES);
-        ss2 = malloc(crypto_kem_BYTES);
-        k2  = malloc(crypto_kem_CIPHERTEXTBYTES);
-
-        //Generate random message 1
-        randombytes(m2, crypto_kem_BYTES);
-        randombytes_init(m2, NULL, 256);
 
         //Generate second secret key
         if( crypto_kem_enc(k2, ss2, pk) != 0){
@@ -76,10 +110,39 @@ int main(){
         //Encrypt second message with shared secret key using one time pad
         otp(c2, crypto_kem_BYTES, m2, ss2);
 
+	fprintBstr(stdout, "c1: ", c1, crypto_kem_BYTES);
+        fprintBstr(stdout, "c2: ", c2, crypto_kem_BYTES);
+	fprintf(stdout, "\n");
 
-	//Perform homomorphic operation on c1 and c2 (OTP)
+
+
+
+	//Decrypt c1 and c2 to show encryption worked
+	unsigned char *n1, *n2;
+	n1 = malloc(crypto_kem_BYTES);
+	n2 = malloc(crypto_kem_BYTES);
+
+	otp(n1, crypto_kem_BYTES, c1, ss1);
+	otp(n2, crypto_kem_BYTES, c2, ss2);
+	
+	fprintBstr(stdout, "Decrypted m1: ", n1, crypto_kem_BYTES);
+        fprintBstr(stdout, "Decrypted m2: ", n2, crypto_kem_BYTES);
+        fprintf(stdout, "\n");
+	
+
+
+
+
+	//Perform homomorphic operation on c1 and c2 (OTP) c' = c1 + c2
 	unsigned char* c = malloc(crypto_kem_BYTES);
 	otp(c, crypto_kem_BYTES, c1, c2);
+
+	fprintBstr(stdout, "c\' = c1 + c2: ", c, crypto_kem_BYTES);
+        fprintf(stdout, "\n");
+
+
+
+
 
 	
 	//Decode kems
@@ -97,23 +160,13 @@ int main(){
 	unsigned char *m = malloc(crypto_kem_BYTES);
 	otp(m, crypto_kem_BYTES, c, _ss1);
 	otp(m, crypto_kem_BYTES, m, ss2);
-	
-	
-	//Show output
-	fprintf(stdout, "\n[Messages]\n");
-	fprintBstr(stdout, "Message 1:\t\t\t", m1, crypto_kem_BYTES);
-	fprintBstr(stdout, "Message 2:\t\t\t", m2, crypto_kem_BYTES);
-	fprintBstr(stdout, "Evaluated Message:\t\t", m, crypto_kem_BYTES);
 
-	unsigned char *expected = malloc(crypto_kem_BYTES);
-	otp(expected, crypto_kem_BYTES, m1, m2);
-	fprintBstr(stdout, "Expected Message:\t\t", expected, crypto_kem_BYTES);
-	
-	fprintf(stdout, "\n[Shared Keys]\n");
-	fprintBstr(stdout, "Shared Secret 1:\t\t", ss1, crypto_kem_BYTES);
-	fprintBstr(stdout, "Shared Secret 2:\t\t", ss2, crypto_kem_BYTES);
-	fprintBstr(stdout, "Evaluated Shared Secret 1:\t", _ss1, crypto_kem_BYTES);
-	fprintBstr(stdout, "Evaluated Shared Secret 2:\t", _ss2, crypto_kem_BYTES);
+	fprintBstr(stdout, "Decrypted c':", m, crypto_kem_BYTES);
+	fprintf(stdout, "\n");
+
+
+
+
 
 	//Free memory
 	free(pk);
@@ -135,7 +188,6 @@ int main(){
 	free(_ss2);
 
 	free(m);
-	free(expected);
 	return 0;
 }
 
