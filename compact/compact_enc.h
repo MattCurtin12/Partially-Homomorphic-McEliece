ed0e2f5821792bcc5a6231bc08fa37d43a3720fe
Matt Curtin <matthew.curtin2@gmail.com>
2022-03-14 00:17:04 -0600

    Corrected plaintext message length to 3

--- compact/compact_enc.h
+++ compact/compact_enc.h
@@ -2,8 +2,8 @@
 #define WEIGHT_M 8                             //the error correcting weight of messages
 #define SYS_N_ORIG 3488                         //N parameter for full message
 #define SYS_T_ORIG 64                           //Weight of full message
-#define SYS_N_ENC (SYS_N_ORIG-2)//- WEIGHT_M)       //N parameter for half message for making e1 and e2
-#define SYS_T_ENC (SYS_T_ORIG/2-2) //- WEIGHT_M)	//Weight of e1 and e2
+#define SYS_N_ENC (SYS_N_ORIG-3)//- WEIGHT_M)       //N parameter for half message for making e1 and e2
+#define SYS_T_ENC (SYS_T_ORIG/2-3) //- WEIGHT_M)	//Weight of e1 and e2
 
 
 void gen_e(unsigned char* e);
