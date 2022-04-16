104709207ea38449245684e475233e1e9bd77753
Matt Curtin <matthew.curtin2@gmail.com>
2022-04-15 20:47:27 -0500

    Fixed timing issue

--- compact/timing.c
+++ compact/timing.c
@@ -61,6 +61,7 @@ int main(){
 
     //Allocate memory for decrypting single messages
     unsigned char *temp = malloc(SYS_N/8);
+    unsigned char *temp2 = malloc(SYS_N/8);
 
     //Random messages
     unsigned char messages[CASES*2];
@@ -72,12 +73,13 @@ int main(){
 
     fprintf(stdout,"Staring test cases\n");
     for(int i = 0; i < CASES; i++){
-        printf("\rTest case: %d...", i);
-        fflush(stdout);
+        printf("Test case: %d...\n", i);
         
         unsigned char m1, m2;
         m1 = messages[i] & 0x07;    //Get last three bits
         m2 = messages[i+1] & 0x07;
+       
+        printf("\tm1: %02X m2: %02X\n", m1, m2);
        
 
         //ENCRYPT
@@ -104,11 +106,14 @@ int main(){
 
         //decrypt
         decrypt(temp, sk+40, c1);
-        decrypt(temp, sk+40, c2);
+        decrypt(temp2, sk+40, c2);
 
         end = clock(); 
         decrypt_single_avg += end - begin;
 
+        printf("\td1: %02X d2: %02X\n", temp[SYS_N/8-1] & 0x07, temp2[SYS_N/8-1] & 0x07); 
+        memset(temp, 0, SYS_N/8);
+        memset(temp2, 0, SYS_N/8);
 
         //Add messages
 	    otp(c, SYND_BYTES, c1, c2); 
@@ -120,6 +125,16 @@ int main(){
 
         end = clock();
         decrypt_summed_avg = end - begin;
+
+        printf("\tExpected: %02X Result: %02X\n\n", m1 ^ m2, temp[SYS_N/8-1] & 0x07);
+        memset(temp, 0, SYS_N/8);
+        memset(c, 0, SYND_BYTES);
+        memset(c1, 0, SYND_BYTES);
+        memset(c2, 0, SYND_BYTES);
+        memset(e1, 0, SYS_N/8);
+        memset(e2, 0, SYS_N/8);
+
+        fflush(stdout);
     }
 
     encrypt_avg /= CASES * 2;
@@ -137,6 +152,7 @@ int main(){
 	free(c2);
 	free(c);
     free(temp);
+    free(temp2);
 
 
 	return 0;
